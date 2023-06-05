27c26baa2385adf5fc7a1027c24b521fc6564562
Sunil Kumar <32964784+Sunilrana1978@users.noreply.github.com>
2023-06-04 18:35:58 -0700

    Update lambda.ts

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,14 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
+
+function asyncTask() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve('connected to database'), 1000);
+  });
+}
 
 let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
+
+  const asyncValue = await asyncTask();
+  console.log(asyncValue);
+
   return serverlessExpress({ app: expressApp });
 }
 
